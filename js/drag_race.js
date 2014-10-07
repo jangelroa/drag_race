3ff29f6a9ec72c4a6305958b47bbaf976d29bcc5
Alex Notov <anotov@gmail.com>
2014-10-06 17:35:33 -0700

    Lesson start

--- js/drag_race.js
+++ js/drag_race.js
@@ -1,9 +1,2 @@
 document.addEventListener("DOMContentLoaded", function(event) {
-  var available = window.AvailableCars,
-      inRace = [
-        new available.GoCar(), 
-        new available.Motorcycle(),
-        new available.Audi(),
-        new available.Truck()
-      ];
 });
--- js/vehicles.js
+++ js/vehicles.js
@@ -1,30 +1,7 @@
 document.addEventListener("DOMContentLoaded", function(event) {
   var Vehicle = function () {
-    this.engine = {
-      running: false
-    };
-    this.start = function() {
-      this.engine.running = true;
-    };
-    this.stop = function() {
-      this.engine.running = false;
-    };
   };
 
   window.AvailableCars = {
-    GoCar: function() {
-    }, 
-    Audi: function() {
-    }, 
-    Truck: function() {
-    },  
-    Motorcycle: function() {
-    }
   };
-
-  window.AvailableCars.GoCar.prototype = 
-  window.AvailableCars.Audi.prototype = 
-  window.AvailableCars.Truck.prototype = 
-  window.AvailableCars.Motorcycle.prototype = 
-    new Vehicle();
 });
