10788e3a732726896ba27e02f94793f3745fc76e
Alex Notov <anotov@gmail.com>
2014-10-06 17:45:33 -0700

    Vehicles have engines that may be started and stopped

--- js/vehicles.js
+++ js/vehicles.js
@@ -1,5 +1,16 @@
 document.addEventListener("DOMContentLoaded", function(event) {
   var Vehicle = function () {
+    var engine = {
+      running: false
+    };
+
+    this.start = function() {
+      engine.running = true;
+    };
+
+    this.stop = function() {
+      engine.running = false;
+    };
   };
 
   window.AvailableCars = {
