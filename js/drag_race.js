79cef682a58de7463692133319eba5b459b6565d
Alex Notov <anotov@gmail.com>
2014-10-06 22:31:31 -0700

    Add a drag strip view object

--- js/drag_race.js
+++ js/drag_race.js
@@ -1,4 +1,3 @@
 document.addEventListener("DOMContentLoaded", function(event) {
-  // how do we get the car to start and show a puff of smoke?
   window.AvailableCars.vehicle.start();
 });
--- js/vehicles.js
+++ js/vehicles.js
@@ -1,4 +1,21 @@
 document.addEventListener("DOMContentLoaded", function(event) {
+  var DragStripView = function() {
+    // what can we do with the drag strip view
+    // in order to be able to work with multiple vehicle views? 
+    //
+    // what else might be the responsibilities of the drag strip
+    // view? 
+    //
+    // how might the vehicle views let the drag strip view
+    // know what to do?
+    //
+    // what might be common between two view objects?
+    //
+    // should view objects exist in this file?
+    // if not, where might they be? 
+    // what do we have to do in index.html?
+  };
+
   var VehicleView = function() {
     var $el = document.getElementById('vehicle1'),
         $exhaust = $el.querySelector('.exhaust'),
