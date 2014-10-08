f5d1a494b04c6a60459b0e0f1fef58a8eaa74059
Alex Notov <anotov@gmail.com>
2014-10-08 16:50:03 -0700

    fix spacing when declaring objects

--- js/drag_race.js
+++ js/drag_race.js
@@ -1,9 +1,9 @@
 document.addEventListener("DOMContentLoaded", function(event) {
   var vehiclesEnteringRace = [
-    { id: "vehicle1", mass: 26, pole: 30}, 
-    { id: "vehicle2", mass: 30, pole: 30}, 
-    { id: "vehicle3", mass: 32, pole: 30}, 
-    { id: "vehicle4", mass: 33, pole: 30}
+    { id: "vehicle1", mass: 26, pole: 30 }, 
+    { id: "vehicle2", mass: 30, pole: 30 }, 
+    { id: "vehicle3", mass: 32, pole: 30 }, 
+    { id: "vehicle4", mass: 33, pole: 30 }
   ];
    
   var vehiclesInRace = new window.Collections.Vehicles(vehiclesEnteringRace);
